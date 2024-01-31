6c0edeb911850ad2fe9e233eac041f7e835af730
Guilherme Oliveira <guilherme.oliveira.mec@gmail.com>
2024-01-31 16:06:20 +0100

    [BREAKING CHANGE]: Adding the Portfolio template from Vercel

--- next.config.js
+++ next.config.js
@@ -1,21 +1,12 @@
+const withNextra = require("nextra")({
+  theme: "nextra-theme-blog",
+  themeConfig: "./theme.config.js",
+  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+});
+
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [{ loader: "@svgr/webpack", 
-              options: { icon: true, 
-                         replaceAttrValues: { '#000': "{props.fill}" }} }],
-    });
+  // any configs you need
+};
 
-    return config;
-  },
-}
-
-module.exports = nextConfig
+module.exports = withNextra(nextConfig);
--- src/infra/myimage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import PropTypes from "prop-types";
-import NextImage from "next/image";
-
-function MyImage(props) {
-  return <NextImage {...props} />;
-}
-
-export default MyImage;
